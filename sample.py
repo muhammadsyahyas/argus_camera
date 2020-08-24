369c68440817c3b2ec1b97bb6ff86aadf27ac12d
Muhammad Shalahuddin Yahya Sunarko <yahya@qlue.id>
2020-08-24 12:03:31 +0700

    Updated sample script

--- sample.py
+++ sample.py
@@ -1,12 +1,20 @@
-#!/usr/bin/env python3
 import cv2
 from argus_camera import ArgusCamera
 
-camera = ArgusCamera()
 
-for i in range(1,1000):
-  image = camera.read()
-  bgr = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-  cv2.imshow("camera", bgr)
-  cv2.waitKey(1)
+def main():
+    camera = ArgusCamera()
 
+    while True:
+        image = camera.read()
+        bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.imshow("camera", bgr)
+        k = cv2.waitKey(1)
+        if k & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            print("Exiting...")
+            break
+
+
+if __name__ == "__main__":
+    main()
