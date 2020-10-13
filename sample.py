d58f721e2e5ffbf950f9b33955b99eaaa45e2e60
Muhammad Shalahuddin Yahya Sunarko <yahya@qlue.id>
2020-10-13 10:49:01 +0700

    extended sample

--- sample.py
+++ sample.py
@@ -3,14 +3,27 @@ from argus_camera import ArgusCamera
 
 
 def main():
-    camera = ArgusCamera()
+    camera = ArgusCamera(
+        device_id=0,
+        stream_resolution=(1640, 1232),
+        video_converter_resolution=(1280, 800),
+        frame_duration_range=(1e9//15, 1e9//15),
+        exposure_time_range=(0, 9999999999999),
+        source_clip_rect=(0., 0., 1., 1.),
+        gain_range=(0., 300.),
+        ae_regions=[[0, 0, 300, 300, 1.0]],
+        sensor_mode=0
+    )
     if not camera.isOpened():
-        raise RuntimeError("Failed initializing camera! (code: %d)" % camera.camera_error_code)
+        raise RuntimeError(
+            "Failed initializing camera! (code: %d)" % camera.camera_error_code)
 
     while True:
         ret, image = camera.read()
         if not ret:
-            print("Failed reading frame from camera! (code: %d)" % camera.read_error_code)
+            print(
+                "Failed reading frame from camera! (code: %d)" %
+                camera.read_error_code)
             continue
         bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         cv2.imshow("camera", bgr)
